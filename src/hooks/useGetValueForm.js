8aef0185b44f4e426d6beb88bc62eaf379400f8e
UmarXLE <mlssaitov@gmail.com>
2023-11-21 18:55:15 +0600

    useGetValueForm.js - конфигурация

--- src/hooks/useGetValueForm.js
+++ src/hooks/useGetValueForm.js
@@ -1,11 +1,15 @@
 
 export const getValueForm = (form) => {
+  //создание formData - и сбор всех данных
   const formData = new FormData(form)
 
+  //получаем все поля из  input-ов и проверяем на их значние
   const values = [...formData.values()]
   const isEmpty = values?.includes("")
 
+  //внедряем все значение полей и отдаем data
   const data = Object.fromEntries(formData)
 
+  //возвращем эти данные
   return {isEmpty,data,values}
 }
